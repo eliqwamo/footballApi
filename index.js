890c39cd3b57eac7b3bda3288f3b0dc3cd110fe9
eliqwamo <eli@qwamo.com>
2022-03-06 17:53:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-
-
 const url = 'mongodb+srv://store_user:Q78kF0frybnjS3nx@cluster0.yrd6o.mongodb.net/store_db?retryWrites=true&w=majority';
 
 const port = 5090;
