f61fdc58b107e88575ca5d705b7137e0ef234756
eliqwamo <eli@qwamo.com>
2022-04-03 20:09:36 +0300

    03042022

--- controllers/actions.js
+++ controllers/actions.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Match = require('../models/match');
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //CREATE
 router.post('/createMatch', (request,response) => {
@@ -89,15 +92,77 @@ router.delete('/removeMatch/:id', async(request, response) => {
 //ACCOUNTS
 router.post('/createUser', async(request,response) => {
     const {firstName,lastName,email,password} = request.body;
-    //1.  is user exist in db?
-    //2.  crytp password
-    //3.  generate passcode
-    //4.  create new account
+    const id = mongoose.Types.ObjectId();
+    //1.  is user exist in db?  
+    User.findOne({email : email})
+    .then(async account => {
+        if(account){
+            return response.status(200).json({
+                message: 'User exist'
+            })
+        } else {
+            //2.  crytp password
+            const hash = await bcryptjs.hash(password,10);
+            //3.  generate passcode 
+            const code = generateCode(100000,999999);
+            //4.  create new account
+            const _user = new User({
+                _id: id,
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hash,
+                passcode: code
+            })
+            return _user.save()
+            .then(account_created => {
+                return response.status(200).json({
+                    message: account_created
+                })
+            })
+        }
+    })
+    .catch(err => {
+        return response.status(500).json({
+            message: err
+        })
+    })
+
+
+
     //5.  response (passcode)
 })
 router.post('/verifyAccount', async(request, response) => {
     const {email,passcode} = request.body;
     //1.  is user exist?
+
+    User.findOne({email : email})
+    .then(account => {
+        if(account){
+            if(parseInt(account.passcode) === parseInt(passcode)) {
+                account.isApproved = true;
+                return account.save()
+                .then(account_updated => {
+                    return response.status(200).json({
+                        message: account_updated
+                    })
+                })
+            } else {
+                return response.status(200).json({
+                    message: 'Passcode not match'
+                })
+            }
+        } else {
+            return response.status(200).json({
+                message: 'User not found'
+            })
+        }
+    })
+    .catch(err => {
+        return response.status(500).json({
+            message: err
+        })
+    })
     //2.  is passcode match
     //3.  update user in db (approved)
     //4.  response
@@ -105,10 +170,60 @@ router.post('/verifyAccount', async(request, response) => {
 router.post('/login', async(request, response) => {
     const {email,password} = request.body;
     //1.  is user exist?
+    User.findOne({email: email})
+    .then(async account => {
+        if(account){
+
+            if(account.isApproved){
+
+                const isMatch = await bcryptjs.compare(password, account.password);
+                if(isMatch){
+
+                    const data = {
+                        account_name: account.firstName + ' ' + account.lastName,
+                        email: account.email,
+                        id: account._id 
+                    }
+                    const token = await jwt.sign(data, 'x1QEDglfUzusifqP5yoBi4ilFcESkcPP');
+                    return response.status(200).json({
+                        message: token
+                    })
+
+                } else {
+                    return response.status(200).json({
+                        message: 'Password not match, please try again'
+                    })
+                }
+
+            } else {
+                return response.status(200).json({
+                    message: 'You need to activate your account'
+                })
+            }
+
+        } else {
+            return response.status(200).json({
+                message: 'User not found'
+            })
+        }
+    })
+    .catch(err => {
+        return response.status(500).json({
+            message: err
+        })
+    })
     //2.  password match
     //3.  user approved
     //4.  generate token
     //5.  response
-
 })
+
+
+
+
+function generateCode(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 module.exports = router;
